8114d897c545e60b7635506a18888ad9886b0f59
Allenboy <32590722+haidragon@users.noreply.github.com>
2019-01-27 18:59:27 +0800

    Add files via upload

---
+++ TestingU/TestingU/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  TestingU
+//
+//  Created by shen on 16/1/20.
+//  Copyright © 2016年 shen. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
